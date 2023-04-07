eae187d23eb3ee8b20a73d03a55ff0659a9cc228
janemumbi730 <janemumbi730@gmail.com>
2023-04-07 04:31:51 -0700

    correction 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (f == -1)
 		return (-1);
