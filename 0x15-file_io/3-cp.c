809bc6181125dab107dfc5d5a043f096dc4266c3
janemumbi730 <janemumbi730@gmail.com>
2023-04-10 08:30:36 -0700

    last correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,11 +13,11 @@ int main(int arc, char *arv[])
 	char buffer[1024];
 
 	if (arc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to \n"), exit(97);
 
 	file_from = open(arv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", arv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arv[1]), exit(98);
 
 	file_to = open(arv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
@@ -34,7 +34,7 @@ int main(int arc, char *arv[])
 	} while (read_file > 0);
 
 	if (read_file == -1)
-	dprintf(STDERR_FILENO, "Error; Can't read from %s\n", arv[1]), exit(98);
+	dprintf(STDERR_FILENO, "Error; Can't read from file %s\n", arv[1]), exit(98);
 
 	if ((close(file_to)) == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
